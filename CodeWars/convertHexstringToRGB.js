007a6515778a9b8c92df24ac14e163132206f5fc
asminkarki012 <asmin@portpro.io>
2023-12-08 16:59:53 +0545

    new problem added

--- CodeWars/convertHexstringToRGB.js
+++ CodeWars/convertHexstringToRGB.js
@@ -1,2 +1,51 @@
 //Convert A Hex String To RGB
-function hexStringToRGB(hexString) {}
+// "#FF9933" --> {r: 255, g: 153, b: 51}
+class HexstringToRGB {
+  constructor() {
+    this.hexToDecimalMapper = {
+      f: 15,
+      e: 14,
+      d: 13,
+      c: 12,
+      b: 11,
+      a: 10,
+    };
+    console.log("RUN CONSTRUCTOR");
+  }
+  hexStringToRGB = (hexString) => {
+    const requiredHex = hexString.toLowerCase().slice(1);
+    const rgb = { r: null, g: null, b: null };
+    const windowSize = 2;
+    const hexPairArray = [];
+    for (let i = 0; i < requiredHex.length; i = i + windowSize) {
+      hexPairArray.push(requiredHex.substring(i, i + windowSize));
+    }
+
+    Object.keys(rgb).forEach((key, index) => {
+      console.log("hexpair array", hexPairArray[index]);
+      rgb[key] = this.hextToDecimal(hexPairArray[index]);
+    });
+    console.log("rgb", rgb);
+    return rgb;
+  };
+
+  hextToDecimal = (hex) => {
+    const hexSplit = hex.split("");
+    const formattedHex = hexSplit.map(
+      (x) => Number(this.hexToDecimalMapper[x]) || parseInt(x)
+    );
+    const hexReducer = formattedHex.reduce(
+      (accumulator, currentValue, currentIndex) => {
+        return (
+          accumulator +
+          currentValue * Math.pow(16, hexSplit.length - 1 - currentIndex)
+        );
+      },
+      0
+    );
+    console.log("hexReducer", hexReducer);
+    return hexReducer;
+  };
+}
+const hexStringToRGBInstance = new HexstringToRGB();
+hexStringToRGBInstance.hexStringToRGB("#FF9933");
---
+++ CodeWars/maximum_sub_array.js
@@ -0,0 +1,45 @@
+// Maximum subarray sum
+
+const maxSequence = function (arr) {
+  if (arr.length === 0) return 0;
+  const maxDigit = Math.max(...arr);
+  const indexOfMaxDigit = arr.indexOf(maxDigit);
+  let leftTraverseIndex = indexOfMaxDigit;
+  let rightTraverseIndex = indexOfMaxDigit;
+  let leftTraversePath = "";
+  let rightTraversePath = "";
+  const pathAndSumArray = [];
+  let leftPathSum = 0;
+  let rightPathSum = 0;
+  while (leftTraverseIndex >= 0 || rightTraverseIndex <= arr.length - 1) {
+    if (leftTraverseIndex !== -1) {
+      const pathAndSumObj = {};
+      leftTraversePath += leftTraverseIndex;
+      leftPathSum += arr[leftTraverseIndex];
+      pathAndSumObj.path = leftTraversePath;
+      pathAndSumObj.sum = leftPathSum;
+      leftTraverseIndex -= 1;
+    }
+    if (rightTraverseIndex !== arr.length + 1) {
+      const pathAndSumObj = {};
+      rightTraversePath += rightTraverseIndex;
+      rightPathSum += arr[rightTraverseIndex];
+      pathAndSumObj.path = rightTraversePath;
+      pathAndSumObj.sum = rightPathSum;
+      pathAndSumArray.push(pathAndSumObj);
+      rightTraverseIndex += 1;
+    }
+  }
+  //   console.log(pathAndSumArray);
+  return pathAndSumArray.reduce((a, currentVal) => {
+    return (a = Math.max(currentVal.sum, a));
+  }, 0);
+};
+
+// console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(
+  maxSequence([
+    7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43,
+  ])
+);
+// should be 6: [4, -1, 2, 1]
