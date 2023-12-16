2d873a608c6bd901929b5e421aca587acb65dcbc
asminkarki012 <asmin@portpro.io>
2023-12-17 02:57:28 +0545

    more problems solved

---
+++ CodeWars/gapInPrimes.js
@@ -0,0 +1,29 @@
+function gap(g, m, n) {
+  for (let i = m; i <= n - g; i++) {
+    if (isPrime(i) && isPrime(i + g) && isSquential(i, i + g)) {
+      return [i, i + g];
+    }
+  }
+  return null;
+}
+
+function isPrime(num) {
+  if (num < 2) return false;
+  const sqrtNum = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= sqrtNum; i++) {
+    if (num % i == 0) return false;
+  }
+  return true;
+}
+
+function isSquential(lowerNum, upperNum) {
+  for (let i = lowerNum + 1; i < upperNum; i++) {
+    if (isPrime(i)) return false;
+  }
+  return true;
+}
+// console.log(gap(2, 100, 110));
+// result:[101, 103]
+console.log(gap(6, 100, 110));
+// console.log(gap(10, 300, 400));
+// console.log(isSquential(359, 367));
--- CodeWars/maximum_sub_array.js
+++ CodeWars/maximum_sub_array.js
@@ -1,45 +1,23 @@
 // Maximum subarray sum
+function maxSequence(arr) {
+  let maxEndingHere = arr[0];
+  let maxSoFar = arr[0];
 
-const maxSequence = function (arr) {
-  if (arr.length === 0) return 0;
-  const maxDigit = Math.max(...arr);
-  const indexOfMaxDigit = arr.indexOf(maxDigit);
-  let leftTraverseIndex = indexOfMaxDigit;
-  let rightTraverseIndex = indexOfMaxDigit;
-  let leftTraversePath = "";
-  let rightTraversePath = "";
-  const pathAndSumArray = [];
-  let leftPathSum = 0;
-  let rightPathSum = 0;
-  while (leftTraverseIndex >= 0 || rightTraverseIndex <= arr.length - 1) {
-    if (leftTraverseIndex !== -1) {
-      const pathAndSumObj = {};
-      leftTraversePath += leftTraverseIndex;
-      leftPathSum += arr[leftTraverseIndex];
-      pathAndSumObj.path = leftTraversePath;
-      pathAndSumObj.sum = leftPathSum;
-      console.log("left pathandSumobj", pathAndSumArray);
-      pathAndSumArray.push(pathAndSumObj);
-      leftTraverseIndex -= 1;
-    }
-    if (rightTraverseIndex !== arr.length + 1) {
-      const pathAndSumObj = {};
-      rightTraversePath += rightTraverseIndex;
-      rightPathSum += arr[rightTraverseIndex];
-      pathAndSumObj.path = rightTraversePath;
-      pathAndSumObj.sum = rightPathSum;
-      console.log("right pathandSumobj", pathAndSumArray);
-      pathAndSumArray.push(pathAndSumObj);
-      rightTraverseIndex += 1;
-    }
+  for (let i = 1; i < arr.length; i++) {
+    maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
   }
-  //   console.log(pathAndSumArray);
-  return pathAndSumArray.reduce((a, currentVal) => {
-    return (a = Math.max(currentVal.sum, a));
-  }, 0);
-};
 
-// console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+  return maxSoFar;
+}
+
+// Example usage:
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// const result = maxSubarraySum(array);
+
+// console.log("Maximum sum subarray:", result);
+
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 console.log(
   maxSequence([
     7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43,
