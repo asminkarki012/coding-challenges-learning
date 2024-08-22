86bd8b1b09df7cfe1016d8a96b34354de38bc756
asminkarki012 <asmin@portpro.io>
2024-08-22 20:11:27 +0545

    genetic-algo some left,exercise 16 completed for stream-adventure

--- CodeWars/bit-genetic-algorithm.js
+++ CodeWars/bit-genetic-algorithm.js
@@ -1,30 +1,38 @@
 //https://www.codewars.com/kata/526f35b9c103314662000007
-var GeneticAlgorithm = function () {};
+const GeneticAlgorithm = function () {};
 
-GeneticAlgorithm.prototype.generate = function(length) {
+GeneticAlgorithm.prototype.generate = function (length) {
   // TODO: Implement the generate method
-  let bitString=''
-  for (let i=0 ;i<length ;i++){
-    bitString+= Math.floor(Math.random() * 2);
+  let bitString = "";
+  for (let i = 0; i < length; i++) {
+    bitString += Math.floor(Math.random() * 2);
   }
   return bitString;
 };
 
 const geneticAlgorithm = new GeneticAlgorithm();
-console.log(geneticAlgorithm.generate(10));
 
-GeneticAlgorithm.prototype.select = function(population, fitnesses) {
+GeneticAlgorithm.prototype.select = function (population, fitnesses) {
   // TODO: Implement the select method
 };
 
-GeneticAlgorithm.prototype.mutate = function(chromosome, p) {
+console.log(geneticAlgorithm.select(["11001111"]));
+// console.log(geneticAlgorithm.generate(10));
+
+GeneticAlgorithm.prototype.mutate = function (chromosome, p) {
   // TODO: Implement the mutate method
 };
 
-GeneticAlgorithm.prototype.crossover = function(chromosome1, chromosome2) {
+GeneticAlgorithm.prototype.crossover = function (chromosome1, chromosome2) {
   // TODO: Implement the crossover method
 };
 
-GeneticAlgorithm.prototype.run = function(fitness, length, p_c, p_m, iterations) {
+GeneticAlgorithm.prototype.run = function (
+  fitness,
+  length,
+  p_c,
+  p_m,
+  iterations
+) {
   // TODO: Implement the run method
 };
